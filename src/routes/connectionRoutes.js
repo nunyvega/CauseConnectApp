b0827b3a5a0155744d4e7c2aba1682344c3e79d8
Alvaro Vega <alvaro.vega@automattic.com>
2023-08-13 15:47:21 +0200

    Rename users page to connections and style it

--- src/routes/connectionRoutes.js
+++ src/routes/connectionRoutes.js
@@ -32,7 +32,7 @@ router.get('/all', isAuthenticated, async (req, res) => {
       user.met = metUserIds.includes(user._id.toString());
     });
 
-    res.render('users', { users, currentUser: req.user });
+    res.render('connections', { users, currentUser: req.user });
   } catch (error) {
     console.error(error);
     res.status(500).send('An error occurred');
