5b4dc62fbec8287bc962691bf4b2ae271251aaed
Alvaro Vega <alvaro.vega@automattic.com>
2023-08-13 15:38:12 +0200

    Add new images and move multerConfig to separate file

---
+++ src/multerConfig.js
@@ -0,0 +1,17 @@
+// multerConfig.js, used to mange user images
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'public/uploads/'));
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix);
+  }
+});
+
+const upload = multer({ storage: storage });
+
+module.exports = upload;
