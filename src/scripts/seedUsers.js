72a0f2b65fc528b44d9cc891b41386befddd40ad
Alvaro Vega <alvaro.vega@automattic.com>
2023-09-18 12:25:26 +0200

    improve seed to add connections and style card in login page

--- src/scripts/seedUsers.js
+++ src/scripts/seedUsers.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose');
 const faker = require('faker');
 const User = require("../models/User");
+const Connection = require("../models/Connection");
 const bcrypt = require('bcrypt');
 
-// Use the same password for every use to test the app faster
+// Use the same password for every user to test the app faster
 const hashedPass = bcrypt.hashSync('admin', 10);
 
 mongoose.connect('mongodb://localhost/CauseConnect', {
     useNewUrlParser: true,
     useUnifiedTopology: true
-}).then(() => {
+})
+.then(() => {
     console.log('Connected to MongoDB');
-    generateUsers(78); // Generate 78 users
-}).catch(err => {
-    console.error('Failed to connect to MongoDB:', err);
-});
-
-User.deleteMany({ username: { $ne: 'romn' } }).then(() => {
+    return User.deleteMany({ username: { $ne: 'admin2' } });
+})
+.then(() => {
     console.log('Deleted all users except for admin.');
-}).catch(err => {
-    console.error('Failed to delete users:', err);
+    return generateUsers(78); // Generate 78 users
+})
+.then(generateConnectionsForAdmin)
+.catch(err => {
+    console.error('Error:', err);
+    mongoose.connection.close();
 });
 
-
-// Randomly generate a number of values for fields with enum values
 function generateRandomItems(path, min, max) {
     let itemsCount = faker.datatype.number({ min: min, max: max });
     let items = [];
@@ -70,12 +71,60 @@ function generateUsers(numberOfUsers) {
         }));
     }
 
-    // Save all users to the database
-    User.insertMany(users).then(docs => {
+    users.push(new User({
+        username: 'admin',
+        password: hashedPass,
+        name: faker.name.findName(),
+        age: faker.datatype.number({ min: 18, max: 80 }),
+        skills: generateRandomItems('skills', 7, 14),
+        interests: generateRandomItems('interests', 6, 14),
+        roles: generateRandomItems('roles', 3, 6),
+        favoriteBook: faker.random.arrayElement(["The Great Gatsby", "Moby Dick", "To Kill a Mockingbird"]),
+        preferredGreeting: generateRandomItems('preferredGreeting', 1, 5),
+        profilePicture: 'https://randomuser.me/api/portraits/men/90.jpg',
+        languagesSpoken: generateRandomItems('languagesSpoken', 1, 4),
+        personalBio: faker.lorem.paragraphs(3),
+        location: faker.address.city(),
+        contactMethods: {
+            email: faker.internet.email(),
+            phone: faker.phone.phoneNumber(),
+        },
+        personalBlogOrWebsite: faker.internet.url(),
+        socialMedia: {
+            facebook: 'facebook.com/admin',
+            twitter: 'twitter.com/admin',
+            instagram: 'instagram.com/admin',
+            linkedin: 'linkedin.com/admin',
+            youtube: 'youtube.com/admin',
+            website: faker.internet.url(),
+        }
+    }));
+
+    return User.insertMany(users).then(docs => {
         console.log(`Inserted ${docs.length} users.`);
-        mongoose.connection.close();
-    }).catch(err => {
-        console.error('Failed to insert users:', err);
     });
 }
 
+function generateConnectionsForAdmin() {
+    return User.findOne({ username: 'admin' }).then(adminUser => {
+        return createConnectionsUsingAdminId(adminUser._id);
+    });
+}
+
+function createConnectionsUsingAdminId(adminId) {
+    return User.aggregate([
+        { $match: { _id: { $ne: adminId } } },
+        { $sample: { size: 43 } }
+    ]).then(users => {
+        const connections = users.map(user => ({
+            user1: adminId,
+            user2: user._id,
+            metDate: new Date()  // Set metDate to the current date/time
+        }));
+
+        return Connection.insertMany(connections).then(docs => {
+            console.log(`Created ${docs.length} connections for the admin user.`);
+            mongoose.connection.close();
+        });
+    });
+}
