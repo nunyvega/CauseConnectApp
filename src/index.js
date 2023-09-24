aaddc55bd470dd098bdc29a7b4bf2c8cdfd74077
Alvaro Vega <alvaro.vega@automattic.com>
2023-09-24 03:39:11 +0200

    Format docs and add check to automatically choose mongodb URI

--- src/index.js
+++ src/index.js
@@ -10,12 +10,12 @@ const User = require("./models/User");
 const flash = require("connect-flash");
 const Connection = require("./models/Connection");
 const { ensureAuthenticated } = require("./middleware/authMiddleware");
-const { spawn } = require('child_process');
-const statisticToShow = require('./public/js/statisticsPool');
-const crypto = require('crypto');
-const secret = crypto.randomBytes(64).toString('hex');
+const { spawn } = require("child_process");
+const statisticToShow = require("./public/js/statisticsPool");
+const crypto = require("crypto");
+const secret = crypto.randomBytes(64).toString("hex");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -24,56 +24,61 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(session({
+app.use(
+  session({
     secret: secret,
     resave: false,
     saveUninitialized: false,
-}));
+  })
+);
 app.use(flash());
 app.use((req, res, next) => {
-    res.locals.error = req.flash("error");
-    next();
+  res.locals.error = req.flash("error");
+  next();
 });
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Passport configuration for local strategy
-passport.use(new LocalStrategy(async (username, password, done) => {
+passport.use(
+  new LocalStrategy(async (username, password, done) => {
     try {
-        const user = await User.findOne({ username });
-        if (!user) return done(null, false);
+      const user = await User.findOne({ username });
+      if (!user) return done(null, false);
 
-        // Compare password with stored hash
-        bcrypt.compare(password, user.password, (err, isMatch) => {
-            if (err) throw err;
-            return isMatch ? done(null, user) : done(null, false);
-        });
+      // Compare password with stored hash
+      bcrypt.compare(password, user.password, (err, isMatch) => {
+        if (err) throw err;
+        return isMatch ? done(null, user) : done(null, false);
+      });
     } catch (err) {
-        return done(err);
+      return done(err);
     }
-}));
+  })
+);
 
 passport.serializeUser((user, done) => done(null, user.id));
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (err) {
-        done(err, null);
-    }
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
 });
 
 // MongoDB connection setup
 const mongoUri = process.env.MONGODB_URI;
 const mongoLocalUri = "mongodb://localhost/CauseConnect";
 
-mongoose.connect(mongoUri, {
+mongoose
+  .connect(mongoUri ? mongoUri : mongoLocalUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-.then(() => console.log("MongoDB Connected"))
-.catch((err) => console.log(err));
+  })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
 
 // View engine setup
 app.set("view engine", "ejs");
@@ -82,11 +87,14 @@ app.set("views", path.join(__dirname, "views"));
 // Routes setup
 app.get("/login", (req, res) => res.render("login"));
 app.get("/register", (req, res) => res.render("register"));
-app.post("/login", passport.authenticate("local", {
+app.post(
+  "/login",
+  passport.authenticate("local", {
     successRedirect: "/",
     failureRedirect: "/login",
     failureFlash: "Invalid username or password.",
-}));
+  })
+);
 
 // Import and use other routes
 const authRoutes = require("./routes/authRoutes");
@@ -101,60 +109,59 @@ app.use("/api", apiRoutes);
 
 // Home route with statistics
 app.get("/", async (req, res) => {
+  // Redirect to login page if not authenticated
+  if (!req.isAuthenticated()) {
+    return res.redirect("/login");
+  }
 
-    // Redirect to login page if not authenticated
-    if( ! req.isAuthenticated() ) {
-        return res.redirect('/login');
-    }
-    
-    const currentUserId = req.user._id;
-    const totalUsers = await User.countDocuments({ _id: { $ne: currentUserId } });
-    const totalConnections = await Connection.countDocuments({
-        $or: [{ user1: currentUserId }, { user2: currentUserId }],
-    });
-    const percentageMet = totalUsers ? (totalConnections / totalUsers) * 100 : 0;
+  const currentUserId = req.user._id;
+  const totalUsers = await User.countDocuments({ _id: { $ne: currentUserId } });
+  const totalConnections = await Connection.countDocuments({
+    $or: [{ user1: currentUserId }, { user2: currentUserId }],
+  });
+  const percentageMet = totalUsers ? (totalConnections / totalUsers) * 100 : 0;
 
-    res.render("home", {
-        percentageMet,
-        totalUsers,
-        metUsers: totalConnections,
-        statisticToShow: statisticToShow.message,
-    });
+  res.render("home", {
+    percentageMet,
+    totalUsers,
+    metUsers: totalConnections,
+    statisticToShow: statisticToShow.message,
+  });
 });
 
 // Logout route
 app.get("/logout", (req, res) => {
-    req.session.destroy(err => {
-        if (err) console.log(err);
-        res.clearCookie("connect.sid");
-        res.redirect("/login");
-    });
+  req.session.destroy((err) => {
+    if (err) console.log(err);
+    res.clearCookie("connect.sid");
+    res.redirect("/login");
+  });
 });
 
 // Route to run seed script
-app.post('/run-seed', (req, res) => {
-    const seedProcess = spawn('node', ['./src/scripts/seedUsers.js']);
+app.post("/run-seed", (req, res) => {
+  const seedProcess = spawn("node", ["./src/scripts/seedUsers.js"]);
 
-    seedProcess.stdout.on('data', data => console.log(`stdout: ${data}`));
-    seedProcess.stderr.on('data', data => console.error(`stderr: ${data}`));
-    seedProcess.on('close', code => {
-        if (code !== 0) {
-            console.log(`seedUsers.js process exited with code ${code}`);
-            return res.status(500).send('Failed to run seed script.');
-        }
-        res.redirect('/login');
-    });
+  seedProcess.stdout.on("data", (data) => console.log(`stdout: ${data}`));
+  seedProcess.stderr.on("data", (data) => console.error(`stderr: ${data}`));
+  seedProcess.on("close", (code) => {
+    if (code !== 0) {
+      console.log(`seedUsers.js process exited with code ${code}`);
+      return res.status(500).send("Failed to run seed script.");
+    }
+    res.redirect("/login");
+  });
 });
 
 // Error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
--- src/scripts/seedUsers.js
+++ src/scripts/seedUsers.js
@@ -1,42 +1,47 @@
-const mongoose = require('mongoose');
-const faker = require('faker');
+const mongoose = require("mongoose");
+const faker = require("faker");
 const User = require("../models/User");
 const Connection = require("../models/Connection");
-const bcrypt = require('bcrypt');
-require('dotenv').config();
+const bcrypt = require("bcrypt");
+require("dotenv").config();
 
 // Use the same password for every user to test the app faster
-const hashedPass = bcrypt.hashSync('admin', 10);
+const hashedPass = bcrypt.hashSync("admin", 10);
 
 // Connect to MongoDB and seed data
 const mongoUri = process.env.MONGODB_URI;
 const mongoLocalUri = "mongodb://localhost/CauseConnect";
 
-mongoose.connect(mongoUri, {
+mongoose
+  .connect(mongoUri ? mongoUri : mongoLocalUri, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => {
-    console.log('Connected to MongoDB');
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
     return clearCollections(); // Call function to clear Users and Connections
-})
-.then(() => {
-    console.log('Deleted all users except for admin.');
+  })
+  .then(() => {
+    console.log("Deleted all users except for admin.");
     return generateUsers(78); // Generate 78 users
-})
-.then(generateConnectionsForAdmin)
-.catch(err => {
-    console.error('Error:', err);
+  })
+  .then(generateConnectionsForAdmin)
+  .catch((err) => {
+    console.error("Error:", err);
     mongoose.connection.close();
-});
+  });
 
 // Clear User and Connection collections
 function clearCollections() {
-    return User.deleteMany({}).then(() => {
-        console.log('All documents in the User collection have been deleted.');
-        return Connection.deleteMany({});
-    }).then(() => {
-        console.log('All documents in the Connection collection have been deleted.');
+  return User.deleteMany({})
+    .then(() => {
+      console.log("All documents in the User collection have been deleted.");
+      return Connection.deleteMany({});
+    })
+    .then(() => {
+      console.log(
+        "All documents in the Connection collection have been deleted."
+      );
     });
 }
 
@@ -44,129 +49,148 @@ function clearCollections() {
  * Generate random items for user attributes
  */
 function generateRandomItems(path, min, max) {
-    let itemsCount = faker.datatype.number({ min: min, max: max });
-    let items = [];
+  let itemsCount = faker.datatype.number({ min: min, max: max });
+  let items = [];
 
-    while (items.length < itemsCount) {
-        const newItem = faker.random.arrayElement(User.schema.path(path).caster.enumValues);
-        // Make sure the item is not already in the array
-        if (!items.includes(newItem)) {
-            items.push(newItem);
-        }
+  while (items.length < itemsCount) {
+    const newItem = faker.random.arrayElement(
+      User.schema.path(path).caster.enumValues
+    );
+    // Make sure the item is not already in the array
+    if (!items.includes(newItem)) {
+      items.push(newItem);
     }
-    return items;
+  }
+  return items;
 }
 
 /**
  * Generate user data and save to the database
  */
 function generateUsers(numberOfUsers) {
-    const users = [];
+  const users = [];
 
-    for (let i = 0; i < numberOfUsers; i++) {
-        let username = faker.internet.userName();
+  for (let i = 0; i < numberOfUsers; i++) {
+    let username = faker.internet.userName();
 
-        users.push(new User({
-            username: username,
-            password: hashedPass,
-            name: faker.name.findName(),
-            age: faker.datatype.number({ min: 18, max: 80 }),
-            skills: generateRandomItems('skills', 7, 14),
-            interests: generateRandomItems('interests', 6, 14),
-            roles: generateRandomItems('roles', 3, 6),
-            favoriteBook: faker.random.arrayElement(["The Great Gatsby", "Moby Dick", "To Kill a Mockingbird"]),
-            preferredGreeting: generateRandomItems('preferredGreeting', 1, 5),
-            profilePicture: i % 2 === 0 ? 'https://randomuser.me/api/portraits/men/' + i + '.jpg' : 'https://randomuser.me/api/portraits/women/' + i + '.jpg',
-            languagesSpoken: generateRandomItems('languagesSpoken', 1, 4),
-            personalBio: faker.lorem.paragraphs(3),
-            location: faker.address.city(),
-            contactMethods: {
-                email: faker.internet.email(),
-                phone: faker.phone.phoneNumber(),
-            },
-            personalBlogOrWebsite: faker.internet.url(),
-            socialMedia: {
-                facebook: 'https://facebook.com/' + username,
-                twitter: 'https://twitter.com/' + username,
-                instagram: 'https://instagram.com/' + username,
-                linkedin: 'https://linkedin.com/' + username,
-                youtube: 'https://youtube.com/' + username,
-                website: faker.internet.url(),
-            }
-        }));
-    }
+    users.push(
+      new User({
+        username: username,
+        password: hashedPass,
+        name: faker.name.findName(),
+        age: faker.datatype.number({ min: 18, max: 80 }),
+        skills: generateRandomItems("skills", 7, 14),
+        interests: generateRandomItems("interests", 6, 14),
+        roles: generateRandomItems("roles", 3, 6),
+        favoriteBook: faker.random.arrayElement([
+          "The Great Gatsby",
+          "Moby Dick",
+          "To Kill a Mockingbird",
+        ]),
+        preferredGreeting: generateRandomItems("preferredGreeting", 1, 5),
+        profilePicture:
+          i % 2 === 0
+            ? "https://randomuser.me/api/portraits/men/" + i + ".jpg"
+            : "https://randomuser.me/api/portraits/women/" + i + ".jpg",
+        languagesSpoken: generateRandomItems("languagesSpoken", 1, 4),
+        personalBio: faker.lorem.paragraphs(3),
+        location: faker.address.city(),
+        contactMethods: {
+          email: faker.internet.email(),
+          phone: faker.phone.phoneNumber(),
+        },
+        personalBlogOrWebsite: faker.internet.url(),
+        socialMedia: {
+          facebook: "https://facebook.com/" + username,
+          twitter: "https://twitter.com/" + username,
+          instagram: "https://instagram.com/" + username,
+          linkedin: "https://linkedin.com/" + username,
+          youtube: "https://youtube.com/" + username,
+          website: faker.internet.url(),
+        },
+      })
+    );
+  }
 
-    // Add admin users
-    users.push(createAdminUser('admin'));
-    users.push(createAdminUser('admin2')); // Add a second admin user
+  // Add admin users
+  users.push(createAdminUser("admin"));
+  users.push(createAdminUser("admin2")); // Add a second admin user
 
-    return User.insertMany(users).then(docs => {
-        console.log(`Inserted ${docs.length} users.`);
-    });
+  return User.insertMany(users).then((docs) => {
+    console.log(`Inserted ${docs.length} users.`);
+  });
 }
 
 /**
  * Helper function to create an admin user
  */
 function createAdminUser(username) {
-    return new User({
-        username: username,
-        isAdmin: true,
-        password: hashedPass,
-        name: faker.name.findName(),
-        age: faker.datatype.number({ min: 18, max: 80 }),
-        skills: generateRandomItems('skills', 7, 14),
-        interests: generateRandomItems('interests', 6, 14),
-        roles: generateRandomItems('roles', 3, 6),
-        favoriteBook: faker.random.arrayElement(["The Great Gatsby", "Moby Dick", "To Kill a Mockingbird"]),
-        preferredGreeting: generateRandomItems('preferredGreeting', 1, 5),
-        profilePicture: 'https://randomuser.me/api/portraits/men/90.jpg',
-        languagesSpoken: generateRandomItems('languagesSpoken', 1, 4),
-        personalBio: faker.lorem.paragraphs(3),
-        location: faker.address.city(),
-        contactMethods: {
-            email: faker.internet.email(),
-            phone: faker.phone.phoneNumber(),
-        },
-        personalBlogOrWebsite: faker.internet.url(),
-        socialMedia: {
-            facebook: 'https://facebook.com/' + username,
-            twitter: 'https://twitter.com/' + username,
-            instagram: 'https://instagram.com/' + username,
-            linkedin: 'https://linkedin.com/' + username,
-            youtube: 'https://youtube.com/' + username,
-            website: faker.internet.url(),
-        }
-    });
+  return new User({
+    username: username,
+    isAdmin: true,
+    password: hashedPass,
+    name: faker.name.findName(),
+    age: faker.datatype.number({ min: 18, max: 80 }),
+    skills: generateRandomItems("skills", 7, 14),
+    interests: generateRandomItems("interests", 6, 14),
+    roles: generateRandomItems("roles", 3, 6),
+    favoriteBook: faker.random.arrayElement([
+      "The Great Gatsby",
+      "Moby Dick",
+      "To Kill a Mockingbird",
+    ]),
+    preferredGreeting: generateRandomItems("preferredGreeting", 1, 5),
+    profilePicture: "https://randomuser.me/api/portraits/men/90.jpg",
+    languagesSpoken: generateRandomItems("languagesSpoken", 1, 4),
+    personalBio: faker.lorem.paragraphs(3),
+    location: faker.address.city(),
+    contactMethods: {
+      email: faker.internet.email(),
+      phone: faker.phone.phoneNumber(),
+    },
+    personalBlogOrWebsite: faker.internet.url(),
+    socialMedia: {
+      facebook: "https://facebook.com/" + username,
+      twitter: "https://twitter.com/" + username,
+      instagram: "https://instagram.com/" + username,
+      linkedin: "https://linkedin.com/" + username,
+      youtube: "https://youtube.com/" + username,
+      website: faker.internet.url(),
+    },
+  });
 }
 
 /**
  * Generate connections for the admin users
  */
 function generateConnectionsForAdmin() {
-    return User.find({ username: { $in: ['admin', 'admin2'] } }).then(adminUsers => {
-        const adminIds = adminUsers.map(user => user._id);
-        return Promise.all(adminIds.map(adminId => createConnectionsUsingAdminId(adminId)));
-    });
+  return User.find({ username: { $in: ["admin", "admin2"] } }).then(
+    (adminUsers) => {
+      const adminIds = adminUsers.map((user) => user._id);
+      return Promise.all(
+        adminIds.map((adminId) => createConnectionsUsingAdminId(adminId))
+      );
+    }
+  );
 }
 
 /**
  * Create connections using the admin user's ID
  */
 function createConnectionsUsingAdminId(adminId) {
-    return User.aggregate([
-        { $match: { _id: { $ne: adminId } } },
-        { $sample: { size: 43 } }
-    ]).then(users => {
-        const connections = users.map(user => ({
-            user1: adminId,
-            user2: user._id,
-            metDate: new Date()  // Set metDate to the current date/time
-        }));
+  return User.aggregate([
+    { $match: { _id: { $ne: adminId } } },
+    { $sample: { size: 43 } },
+  ]).then((users) => {
+    const connections = users.map((user) => ({
+      user1: adminId,
+      user2: user._id,
+      metDate: new Date(), // Set metDate to the current date/time
+    }));
 
-        return Connection.insertMany(connections).then(docs => {
-            console.log(`Created ${docs.length} connections for the admin user.`);
-            mongoose.connection.close();
-        });
+    return Connection.insertMany(connections).then((docs) => {
+      console.log(`Created ${docs.length} connections for the admin user.`);
+      mongoose.connection.close();
     });
+  });
 }
